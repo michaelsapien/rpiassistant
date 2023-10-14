d7b29f9f5c9af82fcca66cd98fb473d62f57bf39
anoopsapien <anoopsapien@gmail.com>
2023-10-14 20:39:45 +0530

    cleaned code

--- tts_to_mqtt_win.py
+++ tts_to_mqtt_win.py
@@ -1,15 +1,19 @@
-import paho.mqtt.client as mqtt
 import speech_recognition as sr
 import openai
+import pyttsx3
+import sys
 
-
+bot_name='panvi'
 openai.api_key = "xxx"
-messages = [{"role": "system", "content": "Your name is pybot and give answers in 2 lines"}]
 
-# MQTT Settings
-MQTT_BROKER = "192.168.1.14"
-MQTT_PORT = 1883
-MQTT_TOPIC = "speechToTextTopic"
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+rate = engine.getProperty('rate')
+volume = engine.getProperty('volume')
+
+
+messages = [{"role": "system", "content": "Your name is "+bot_name+" you were developed by stemx in June 2016. STEMx group were one of the first to introduce cutting edge technology in k-12 education and trainings since 2016.Two years back we introduced hands-on LEGO Robotics, Coding, and early concepts of STEM. Today our online STEM program has no limit to region, as we have students from India, Dubai, USA, Singapore, England, Nepal, SA, Canada, and Japan. 1,00,000+ students across India experienced STEM Education through STEMx 100+ Schools across India 10+ Channel partners across India 4 STEMx Studios across south India Online STEM Program in 7+ countries Students won national & international awards in Robotics  Competition Strong STEMx alumni community For more than 7+ Years  Founders of STEMx have been working with parents, teachers, schools, and governments to deliver playful learning experiences that bring  STEM subjects like Robotics, Coding, 3D Printing, Electronics & Drones to life  for the students and make learning fun and impactful and you should support Institute by giving Live campus tour for new prospects to parents, Can be an assistant teacher in classroom,Take students attendance and institute's receptionist"}]#give answers in two sentences
+
 def get_response(user_input):
     messages.append({"role": "user", "content": user_input})
     response = openai.ChatCompletion.create(
@@ -20,12 +24,6 @@ def get_response(user_input):
     messages.append({"role": "assistant", "content": ChatGPT_reply})
     return ChatGPT_reply
 
-def send_mqtt_message(message):
-    client = mqtt.Client("WindowsClient")
-    client.connect(MQTT_BROKER, MQTT_PORT, 60)
-    client.publish(MQTT_TOPIC, message)
-    client.disconnect()
-
 def main():
     recognizer = sr.Recognizer()
     microphone = sr.Microphone()
@@ -35,7 +33,6 @@ def main():
     with microphone as source:
         recognizer.adjust_for_ambient_noise(source, duration=5)
         print("Microphone calibrated")
-    
     while True:
         print("Say something:")
         with microphone as source:
@@ -44,11 +41,16 @@ def main():
                 print("Recognizing...")
                 # Use Google Web Speech API
                 text = recognizer.recognize_google(audio)
-                if "pybot" in text.lower():           
-                    response_from_openai = get_response(text)
+                if bot_name in text.lower():           
+                    if('exit' in text.lower()):
+                        sys.exit()
+                    response_from_openai =  (text)
                     print("Q: " + text)
                     print("A: " + response_from_openai)
-                    send_mqtt_message(response_from_openai)
+                    engine.setProperty('rate', 120)
+                    engine.setProperty('volume', volume)
+                    engine.say(response_from_openai)
+                    engine.runAndWait()
             except sr.WaitTimeoutError:
                 print("Speech not detected. Listening again...")
             except sr.UnknownValueError:
