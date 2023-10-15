4e9836ee077d2108698ae27841a7bf6859fdbe27
anoopsapien <anoopsapien@gmail.com>
2023-10-16 02:10:52 +0530

    updates

--- flask_server.py
+++ flask_server.py
@@ -1,21 +1,18 @@
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-api = Api(app)
 
+@app.route('/post_endpoint', methods=['POST'])
+def post_example():
+    # Get JSON data
+    data = request.get_json()
 
-class home(Resource):
-    def get(self):
-        return jsonify(
-            {
-                'Message': 'flaskserver is running',
-                'description': 'check api documentation for details',
-                'status': 200
-            }
-        )
-
-api.add_resource(home, '/')
+    # Check if "message" key exists in JSON and it's not empty
+    if 'message' in data and data['message']:
+        print(f"Message received: {data['message']}")
+        return jsonify(success=True, message="Data received"), 200
+    else:
+        return jsonify(success=False, message="No message provided"), 400
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
