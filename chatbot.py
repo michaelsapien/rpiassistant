3e7e6900f6a7ae5d48da4a823fa5d28fffe2ab0e
anoopsapien <anoopsapien@gmail.com>
2023-10-15 22:26:30 +0530

    updated .ignore

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,14 @@
-from gtts import gTTS
-import os
 import speech_recognition as sr
 import openai
+import pyttsx3
 import sys
-
 wakeword='chatbot'
 openai.api_key = "xxx"
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+rate = engine.getProperty('rate')
+volume = engine.getProperty('volume')
 
 messages = [{"role": "system", "content": "Your name is "+wakeword+" you were developed by stem-x in June 2016 and give answers in two sentences"}]
 
@@ -19,11 +22,6 @@ def get_response(user_input):
     messages.append({"role": "assistant", "content": ChatGPT_reply})
     return ChatGPT_reply
 
-def speak_gtts(text, lang='en'):
-    tts = gTTS(text=text, lang=lang, slow=False)
-    tts.save("response.mp3")
-    os.system("mpg321 response.mp3")
-
 def main():
     recognizer = sr.Recognizer()
     microphone = sr.Microphone()
@@ -33,24 +31,25 @@ def main():
     with microphone as source:
         recognizer.adjust_for_ambient_noise(source, duration=5)
         print("Microphone calibrated")
-    
     while True:
         print("Say something:")
         with microphone as source:
             try:
                 audio = recognizer.listen(source, timeout=10)
                 print("Recognizing...")
+                # Use Google Web Speech API
                 text = recognizer.recognize_google(audio)
-                
-                if wakeword in text.lower():
+                if wakeword in text.lower():           
                     if('exit' in text.lower()):
                         print("exiting...")
                         sys.exit()                    
                     print("Q: " + text)
                     response_from_openai = get_response(text)
                     print("A: " + response_from_openai)
-                    speak_gtts(response_from_openai)  # Using gTTS instead of pyttsx3
-
+                    engine.setProperty('rate', 120)
+                    engine.setProperty('volume', volume)
+                    engine.say(response_from_openai)
+                    engine.runAndWait()
             except sr.WaitTimeoutError:
                 print("Speech not detected. Listening again...")
             except sr.UnknownValueError:
